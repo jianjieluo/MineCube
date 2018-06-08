242615a2b0701ec0c594b7573fdaae2361750729
longjj <luojj26@mail2.sysu.edu.cn>
2018-06-08 10:49:18 +0800

    add easy phong shader

--- src/Shader/Phongfs.fs
+++ src/Shader/Phongfs.fs
@@ -4,6 +4,10 @@ out vec4 FragColor;
 in vec3 FragPos;
 in vec3 OurColor;
 in vec3 Normal;
+in vec4 FragPosLightSpace;
+
+uniform sampler2D diffuseTexture;
+uniform sampler2D shadowMap;
 
 uniform vec3 lightPos; 
 uniform vec3 viewPos;
@@ -26,6 +30,46 @@ struct Light {
 uniform Material material;
 uniform Light light;
 
+
+float ShadowCalculation(vec4 fragPosLightSpace)
+{
+    // perform perspective divide
+    vec3 projCoords = fragPosLightSpace.xyz / fragPosLightSpace.w;
+
+    // transform to [0,1] range
+    projCoords = projCoords * 0.5 + 0.5;
+
+    // get closest depth value from light's perspective (using [0,1] range fragPosLight as coords)
+    float closestDepth = texture(shadowMap, projCoords.xy).r; 
+
+    // get depth of current fragment from light's perspective
+    float currentDepth = projCoords.z;
+
+    // calculate bias (based on depth map resolution and slope)
+    vec3 normal = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - FragPos);
+    float bias = max(0.05 * (1.0 - dot(normal, lightDir)), 0.005);
+
+    // PCF
+    float shadow = 0.0;
+    vec2 texelSize = 1.0 / textureSize(shadowMap, 0);
+    for(int x = -1; x <= 1; ++x)
+    {
+        for(int y = -1; y <= 1; ++y)
+        {
+            float pcfDepth = texture(shadowMap, projCoords.xy + vec2(x, y) * texelSize).r; 
+            shadow += currentDepth - bias > pcfDepth  ? 1.0 : 0.0;        
+        }    
+    }
+    shadow /= 9.0;
+    
+    // keep the shadow at 0.0 when outside the far_plane region of the light's frustum.
+    if(projCoords.z > 1.0)
+        shadow = 0.0;
+        
+    return shadow;
+}
+
 void main()
 {
     // ambient
@@ -42,7 +86,10 @@ void main()
     vec3 reflectDir = reflect(-lightDir, norm);  
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
     vec3 specular = light.specular * (spec * material.specular);
-        
-    vec3 result = ambient + diffuse + specular;
-    FragColor = vec4(result, 1.0);
+    
+    // calculate shadow
+    float shadow = ShadowCalculation(FragPosLightSpace);                      
+    vec3 lighting = (ambient + (1.0 - shadow) * (diffuse + specular)) * OurColor;    
+
+    FragColor = vec4(lighting, 1.0);
 } 
