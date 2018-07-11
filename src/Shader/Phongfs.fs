a6855594eebc5fc5d5bdfb09b246ffecd24315c1
longjj <luojj26@mail2.sysu.edu.cn>
2018-07-11 10:24:40 +0800

    apply blinn-phong rather than traditional phong

--- src/Shader/Phongfs.fs
+++ src/Shader/Phongfs.fs
@@ -85,8 +85,8 @@ void main()
     
     // specular
     vec3 viewDir = normalize(viewPos - FragPos);
-    vec3 reflectDir = reflect(-lightDir, norm);  
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    vec3 halfwayDir = normalize(lightDir + viewDir);
+    float spec = pow(max(dot(norm, halfwayDir), 0.0), material.shininess);
     vec3 specular = light.specular * (spec * material.specular);
     
     // calculate shadow
