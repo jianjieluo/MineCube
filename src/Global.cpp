bf8dc48b02051fe5481e1425fe87b607b6d90d07
Ace-0 <w674901087@163.com>
2018-07-11 19:29:58 +0800

    clean up the code in picking.cpp

--- src/Global.cpp
+++ src/Global.cpp
@@ -7,7 +7,7 @@ float cubes_color[4] = { 0.76f, 0.78f, 0.96f, 1.0f };
 float background_color[4] = { 0.86f, 0.94f, 0.96f, 1.0f };
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
-unsigned int numPerEdge = 20;
+unsigned int numPerEdge = 10;
 int mode = ERASE_MODE;
 
 // interactive variables
